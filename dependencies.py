b8e2c3563353194b33acb46154df212633f5e6d2
Hector G <hmgarcia06@gmail.com>
2023-10-27 22:58:30 -0400

    Auto assigned argument value for get_token_header function dependency

--- dependencies.py
+++ dependencies.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import Header, HTTPException
 
 
-async def get_token_header(x_token: Annotated[str, Header()]):
+async def get_token_header(x_token: Annotated[str, Header()] = None):
     pass
     # if x_token != "fake-super-secret-token":
     #     raise HTTPException(status_code=400, detail="X-Token header invalid")
