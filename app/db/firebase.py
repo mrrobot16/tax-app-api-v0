b009dffc49cb756815d085a52e7f14c6ea877b6c
Hector G <hmgarcia06@gmail.com>
2023-11-02 13:34:36 -0400

    removed at the moment unneeded env variables

--- app/db/firebase.py
+++ app/db/firebase.py
@@ -1,26 +1,27 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from config import config
+from config import env_variables
 
+#  NOTE: Some variables are not needed for firestore.
 FIREBASE_CREDENTIALS = {
-    'type': config.FIREBASE_ACCOUNT_TYPE,
-    'project_id': config.FIREBASE_PROJECT_ID,
+    'type': env_variables.FIREBASE_ACCOUNT_TYPE,
+    'project_id': env_variables.FIREBASE_PROJECT_ID,
     # 'private_key_id': config.FIREBASE_PRIVATE_KEY_ID,
-    'private_key': config.FIREBASE_PRIVATE_KEY,
-    'client_email': config.FIREBASE_CLIENT_EMAIL,
-    # 'client_id': config.FIREBASE_CLIENT_ID,
-    # 'auth_uri': config.FIREBASE_AUTH_URI,
-    'token_uri': config.FIREBASE_TOKEN_URI,
-    # 'auth_provider_x509_cert_url': config.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    # 'client_x509_cert_url': config.FIREBASE_CLIENT_X509_CERT_URL,
-    # 'universe_domain': config.FIREBASE_UNIVERSE_DOMAIN
+    'private_key': env_variables.FIREBASE_PRIVATE_KEY,
+    'client_email': env_variables.FIREBASE_CLIENT_EMAIL,
+    # 'client_id': env_variables.FIREBASE_CLIENT_ID,
+    # 'auth_uri': env_variables.FIREBASE_AUTH_URI,
+    'token_uri': env_variables.FIREBASE_TOKEN_URI,
+    # 'auth_provider_x509_cert_url': env_variables.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    # 'client_x509_cert_url': env_variables.FIREBASE_CLIENT_X509_CERT_URL,
+    # 'universe_domain': env_variables.FIREBASE_UNIVERSE_DOMAIN
 }
 
-# Initialize Firebase Admin SDK
+# NOTE: Initialize Firebase Admin SDK
 credentials = credentials.Certificate(FIREBASE_CREDENTIALS)
 firebase_admin.initialize_app(credentials)
 
 client = firestore.client()
 
-# Firestore database instance
+# NOTE: Firestore database instance
 firestore_db  = client
