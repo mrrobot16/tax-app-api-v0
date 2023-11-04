7c4f7c982bf594c5b1c69ae0f88a9f2b6bec0231
Hector G <hmgarcia06@gmail.com>
2023-11-04 18:59:46 -0400

    add properties to Conversation and Message models

--- app/models/conversation.py
+++ app/models/conversation.py
@@ -1,4 +1,20 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+from app.models.message import MessageGroupModel, MessageModel
 
 class ConversationModel(BaseModel):
-    pass
+    id: str
+    user_id: str
+    name: str
+    messages: list[MessageModel] = []
+    message_groups: list[MessageGroupModel] = []
+    active: bool = True
+    created_at: datetime
+    updated_at: datetime
+
+    def add_message(self, message: MessageModel):
+        self.messages.append(message)
+    
+    def add_message_group(self, message_group: MessageGroupModel):
+        self.message_groups.append(message_group)
--- app/models/message.py
+++ app/models/message.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+from app.enums.user import UserRole
 
 class MessageModel(BaseModel):
     id: str
     user_id: str
     conversation_id: str
     content: str
-    role: str
-    created_at: str
+    role: UserRole
+    created_at: datetime
 
 class MessageGroupModel(BaseModel):
     id: str
     user_id: str
     conversation_id: str
     messages: list[MessageModel]
-    created_at: str
