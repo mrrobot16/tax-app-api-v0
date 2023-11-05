26863bd5df7609226b8bb63b419ff3299a51f080
Hector G <hmgarcia06@gmail.com>
2023-11-04 21:52:25 -0400

    Created test for chat completion

--- tests/unit/services/test_openai.py
+++ tests/unit/services/test_openai.py
@@ -1,5 +1,5 @@
-from services.openai import chat_completion
+from tests.fixtures.openai import openai_service
 
-def test_chat_completion():
-    response = chat_completion("What is a 1040?")
+def test_chat_completion(openai_service):
+    response = openai_service.chat_completion()
     assert response is not None
