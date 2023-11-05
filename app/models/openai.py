f7dcbbef92a4a60c7f40350a4de085b9fe92a34c
Hector G <hmgarcia06@gmail.com>
2023-11-04 23:26:17 -0400

    Simplified OpenAI service and model

--- app/models/openai.py
+++ app/models/openai.py
@@ -1,56 +1,10 @@
-# from pydantic import BaseModel
+from pydantic import BaseModel
 
-
-
-# import openai
-# from config import OPENAI_API_KEY_DEV
-# from constants.openai import OPENAI_ENGINE, OPENAI_TEMPERATURE, OPENAI_MAX_TOKENS, OPENAI_ASSISTANT_PROMPT, OPENAI_SYSTEM_PROMPT, OPENAI_USER_PROMPT
-# from utils import generate_timestamp, generate_unique_id
-# class OpenAI:
-
-#     def __init__(self, engine = OPENAI_ENGINE, keys = OPENAI_API_KEY_DEV, temperature = OPENAI_TEMPERATURE,  max_tokens = OPENAI_MAX_TOKENS):
-#         self.id = generate_unique_id()
-#         self.keys = keys
-#         self.engine = engine
-#         self.temperature = temperature
-#         self.max_tokens = max_tokens
-#         openai.api_key = self.keys
-
-#     def chat_completion(self, prompt = OPENAI_USER_PROMPT['content'], system_prompt = OPENAI_SYSTEM_PROMPT, assistant_prompt = OPENAI_ASSISTANT_PROMPT): 
-#         self.created_at = generate_timestamp()
-#         user_prompt = prompt.encode(encoding = 'ASCII', errors = 'ignore').decode() + ". " + OPENAI_USER_PROMPT['content']
-#         try:
-#             openai_response = openai.ChatCompletion.create(
-#                 model = self.engine,
-#                 messages = [
-#                     system_prompt,
-#                     assistant_prompt,
-#                     { "role": "user", "content": user_prompt }
-#                 ],
-#                 temperature = self.temperature,
-#                 max_tokens = self.max_tokens
-#             )
-#             openai_response = OpenAIResponseModel(openai_response).to_dict()
-#             content = openai_response['choices'][0]['message']['content']
-#             role = openai_response['choices'][0]['message']['role']
-#             response = {
-#                 "api": {
-#                     "id": self.id,
-#                     "created_at": self.created_at,
-#                     "prompt": user_prompt,
-#                     "content": content,
-#                     "role": role,
-#                 },
-#                 "openai": openai_response 
-#             }
-#             return response
-#         except Exception as error:
-#             return f"chat_completion error: {error}"
-
-# class OpenAIResponseModel(BaseModel):
-#     id: str # str ie: "chatcmpl-89jaHxop3jAcGGTzeSZhpoJzP1g0y"
-#     object: str # str ie: "chat.completion"
-#     created: int # int ie: 1697330869
-#     model: str # str ie: "gpt-4"
-#     choices: list # array [ { index: int, message: { role: str ie: assistant, content: str }, finish_reason: str } ]
-#     usage: dict # dict { prompt_tokens: int, completion_tokens: int, total_tokens: int }
+# NOTE: The current response for ChatCompletion is formatted like this.
+class OpenAIChatCompletionResponseModel(BaseModel):
+    id: str # str ie: "chatcmpl-89jaHxop3jAcGGTzeSZhpoJzP1g0y"
+    object: str # str ie: "chat.completion"
+    created: int # int ie: 1697330869
+    model: str # str ie: "gpt-4"
+    choices: list # array [ { index: int, message: { role: str ie: assistant, content: str }, finish_reason: str } ]
+    usage: dict # dict { prompt_tokens: int, completion_tokens: int, total_tokens: int }
--- app/services/openai.py
+++ app/services/openai.py
@@ -1,52 +1,44 @@
 import openai
 
-from app.config import env_variables
-from app.constants.openai import OPENAI_ENGINE, OPENAI_TEMPERATURE, OPENAI_MAX_TOKENS, OPENAI_ASSISTANT_PROMPT, OPENAI_SYSTEM_PROMPT, OPENAI_USER_PROMPT
-from app.utils import generate_timestamp
-
-OPENAI_API_KEY_DEV = env_variables.OPENAI_API_KEY_DEV
+from app.constants.openai import (
+    OPENAI_ENGINE, 
+    OPENAI_TEMPERATURE, OPENAI_MAX_TOKENS, 
+    OPENAI_ASSISTANT_PROMPT, OPENAI_SYSTEM_PROMPT, OPENAI_USER_PROMPT, 
+    OPENAI_API_KEY_DEV
+)
+from app.models.openai import OpenAIChatCompletionResponseModel
 
 class OpenAIService:
 
-    def __init__(self, engine = OPENAI_ENGINE, keys = OPENAI_API_KEY_DEV, temperature = OPENAI_TEMPERATURE,  max_tokens = OPENAI_MAX_TOKENS):
-        self.keys = keys
-        self.engine = engine
-        self.temperature = temperature
-        self.max_tokens = max_tokens
-        openai.api_key = self.keys
+    def __init__(self, keys = OPENAI_API_KEY_DEV):
+        openai.api_key = keys
 
-    def chat_completion(self, prompt = OPENAI_USER_PROMPT['content'], system_prompt = OPENAI_SYSTEM_PROMPT, assistant_prompt = OPENAI_ASSISTANT_PROMPT):
-        self.created_at = generate_timestamp()
-        user_prompt = prompt.encode(encoding = 'ASCII', errors = 'ignore').decode() + ". " + OPENAI_USER_PROMPT['content']
+    def chat_completion(
+            self,
+            prompt = OPENAI_USER_PROMPT['content'], 
+            engine = OPENAI_ENGINE,
+            system_prompt = OPENAI_SYSTEM_PROMPT, 
+            assistant_prompt = OPENAI_ASSISTANT_PROMPT,
+            temperature = OPENAI_TEMPERATURE,  
+            max_tokens = OPENAI_MAX_TOKENS,
+    ):
         try:
-            openai_response = openai.ChatCompletion.create(
-                model = self.engine,
+            # NOTE: prompt.encode().decode() is used to remove non-ASCII characters from the prompt.
+            user_prompt = {
+                "role": "user",
+                "content": prompt.encode(encoding = 'ASCII', errors = 'ignore').decode() + ". "
+            }
+            response = openai.ChatCompletion.create(
+                model = engine,
                 messages = [
                     system_prompt,
                     assistant_prompt,
-                    { "role": "user", "content": user_prompt }
+                    user_prompt,
                 ],
-                temperature = self.temperature,
-                max_tokens = self.max_tokens
+                temperature = temperature,
+                max_tokens = max_tokens
             )
-            print('openai_response', openai_response)
-            return openai_response
-            # openai_response = OpenAIResponse(**openai_response).to_dict()
-            # content = openai_response['choices'][0]['message']['content']
-            # role = openai_response['choices'][0]['message']['role']
-            # response = {
-            #     "api": {
-            #         "id": self.id,
-            #         "created_at": self.created_at,
-            #         "prompt": user_prompt,
-            #         "content": content,
-            #         "role": role,
-            #     },
-            #     "openai": openai_response 
-            # }
-            # return response
+            openai_response_model = OpenAIChatCompletionResponseModel(**response)
+            return openai_response_model
         except Exception as error:
             return f"chat_completion error: {error}"
-        
-def get_openai_service():
-    return OpenAIService()
