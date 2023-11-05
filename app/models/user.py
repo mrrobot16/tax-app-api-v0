7f4d18eaa9d76b0b321f2fca395a3f82e26470b2
Hector G <hmgarcia06@gmail.com>
2023-11-05 17:09:03 -0500

    Added optional type to some properties in UserModel

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,27 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
+from typing import List, Optional
 
 from app.models.conversation import ConversationModel
-from app.enums.user  import UserRole, UserAuthType
+from app.enums.user import UserAuthType, UserRole
 
 class UserModel(BaseModel):
     id: str
-    name: str | None = None
-    email: EmailStr | None = None
-    conversations: list[ConversationModel] = []
-    role: UserRole = UserRole.USER
+    name: Optional[str] = None
+    email: EmailStr
+    conversations: List[ConversationModel] = []
     active: bool = True
-    auth_type: UserAuthType = UserAuthType.EMAIL_PASSWORD
+    auth_type: UserAuthType = Field(default = UserAuthType.EMAIL_PASSWORD, alias = 'auth_type')
+    role: UserRole = Field(default = UserRole.USER, alias = 'role')
     created_at: datetime
     updated_at: datetime
 
+    class Config:
+        use_enum_values = True
+        populate_by_name = True
+        
+
 class UserPrivateModel(UserModel):
-    password: str | None = None
+    password: Optional[str] = None
+    class Config(UserModel.Config):
+        pass
