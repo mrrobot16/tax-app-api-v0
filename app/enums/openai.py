8b4d43f98d0d90f22cf8a5097e29f2813a0cdfa4
Hector G <hmgarcia06@gmail.com>
2023-11-04 23:58:35 -0400

    Added enum for openai

---
+++ app/enums/openai.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class OpenAIModel(Enum):
+    GPT_4 = 'gpt-4-0613'
+    GPT_4_32K = 'gpt-4-32k-0613'
+    GPT_3_5_TURBO = 'gpt-3.5-turbo-0613'
+    GPT_3_5_TURBO_16K = 'gpt-3.5-turbo-16k-0613'
+
+class OpenAIObjectType(Enum):
+    CHAT_COMPLETION = 'chat.completion'
+    TEXT_COMPLETION = 'text_completion'
+    TEXT_MODERATION = 'text-moderation-005'
