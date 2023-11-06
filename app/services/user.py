aee2ad18326927ebca2c8855abb601849fb539d3
Hector G <hmgarcia06@gmail.com>
2023-11-05 20:42:20 -0500

    Now all UserService methods return a dict from a firebase document

--- app/services/user.py
+++ app/services/user.py
@@ -1,11 +1,7 @@
-import warnings
-
 from app.db.firebase import users_collection
-from app.models.user import UserPrivateModel, UserModel
 from app.enums.user import UserAuthType, UserRole
 from app.utils import generate_timestamp, generate_unique_id, hash_password
 from app.utils.firebase import convert_doc_refs
-from app.constants import filter_warning_message
 
 class UserService:
 
@@ -17,11 +13,6 @@ class UserService:
         return user_list_doc_refs
 
     def get(id):
-        warnings.filterwarnings(
-            "ignore", 
-            category = UserWarning, 
-            message = filter_warning_message
-        )
         # Query the collection where 'id' is equal to user_id
         users = users_collection.where('id', '==', id).limit(1).stream()
 
@@ -32,8 +23,8 @@ class UserService:
         # NOTE: collection_ref.where returns an array of documents.
         # We only want the first document in the array.
         user = user_list[0] if user_list else None
-        return user
-        # return users_collection.document(id).get().to_dict()
+        user_doc_ref = convert_doc_refs(user)
+        return user_doc_ref
     
     def new(
             name = "test_name",
@@ -61,27 +52,34 @@ class UserService:
             'updated_at': updated_at
         }
 
-        user_model = UserModel(**user_data)
-        user_private_model = UserPrivateModel(
-            **user_model.dict(),
-            password = user_data["password"]
-        )
+        user_ref = users_collection.document(user_data["id"])
+        user_ref.set(user_data)
+        # return user_data
+        # After setting the data, get the document back as a snapshot and convert it to a dictionary
+        user_snapshot = user_ref.get()
+        if user_snapshot.exists:
+            return user_snapshot.to_dict()
+        else:
+            return None
 
-        # Add the user to the 'users' collection
-        user_ref = users_collection.document(user_private_model.id)
-        user_ref.set(user_private_model.dict())
-        return user_ref
-    
-    # NOTE: data argument should only contain name and email.
-    # TODO: Need to figure out a way to validate that data only can contain those 2 fields.
+    # NOTE: Need to figure out a way to validate that data argument 
+    # can only contain name and email.
     def update(id, data):
         update_data = {
             **data,
             'updated_at': generate_timestamp()
         }
-        users_ref = users_collection.document(id)
-        users_ref.update(update_data)
-        return data
+        user_ref = users_collection.document(id)
+        user_ref.update(update_data)
+
+        # Now read the updated document
+        updated_document = user_ref.get()
+        if updated_document.exists:
+            user_doc_ref = convert_doc_refs(updated_document.to_dict())
+            return user_doc_ref
+        else:
+            # Handle the case where the document does not exist
+            return None
     
     def delete(id):
         users_ref = users_collection.document(id)
@@ -94,7 +92,10 @@ class UserService:
             'active': False,
             'update_at': generate_timestamp()
         })
-        return id
+        # Retrieve the updated document
+        user_doc = user_ref.get()
+        # Return the document data as a dictionary
+        return user_doc.to_dict()
     
     def activate(id):
         user_ref = users_collection.document(id)
@@ -102,4 +103,7 @@ class UserService:
             'active': True,
             'update_at': generate_timestamp()
         })
-        return id
+        # Retrieve the updated document
+        user_doc = user_ref.get()
+        # Return the document data as a dictionary
+        return user_doc.to_dict()
