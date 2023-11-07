5cc391f40ffd99fc2c38113226c8bc2ea21b6e0f
Hector G <hmgarcia06@gmail.com>
2023-11-07 03:26:35 -0500

    Increased timeout to test gpt4-1106-preview

--- app/enums/openai.py
+++ app/enums/openai.py
@@ -3,6 +3,7 @@ from enum import Enum
 class OpenAIModel(Enum):
     GPT_4 = 'gpt-4-0613'
     GPT_4_32K = 'gpt-4-32k-0613'
+    GPT_4_1106 = 'gpt-4-1106-preview'
     GPT_3_5_TURBO = 'gpt-3.5-turbo-0613'
     GPT_3_5_TURBO_16K = 'gpt-3.5-turbo-16k-0613'
 
