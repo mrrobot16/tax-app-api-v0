86f31483ffb5a6a2085aeee24844c8ad448c666a
Hector G <hmgarcia06@gmail.com>
2023-11-07 23:24:19 -0500

    User model now has id and datetimes as optionals

--- app/models/user.py
+++ app/models/user.py
@@ -6,15 +6,15 @@ from app.models.conversation import ConversationModel
 from app.enums.user import UserAuthType, UserRole
 
 class UserModel(BaseModel):
-    id: str
+    id: Optional[str] = None
     name: Optional[str] = None
     email: EmailStr
     conversations: List[ConversationModel] = []
     active: bool = True
     auth_type: UserAuthType = Field(default = 'email-password', alias = 'auth_type')
     role: UserRole = Field(default = 'user', alias = 'role')
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
     class Config:
         use_enum_values = True
