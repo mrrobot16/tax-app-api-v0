b972395a97d9a35a04e6cb9e59bf3fc3d14f968c
Hector G <hmgarcia06@gmail.com>
2023-11-08 21:00:04 -0500

    Created services for crud firebase operations

---
+++ app/services/conversation.py
@@ -0,0 +1,114 @@
+from app.db.firebase import conversations_collection
+from app.models.conversation import ConversationModel
+from app.utils import generate_timestamp, generate_unique_id, hash_password
+from app.utils.firebase import convert_doc_refs
+
+class ConversationService:
+
+    def get_all():
+        conversations = conversations_collection.stream()
+        # Convert each DocumentSnapshot to a dictionary and add it to a list
+        conversations_list = [conversation.to_dict() for conversation in conversations]
+        conversations_list_doc_refs = convert_doc_refs(conversations_list)
+        return conversations_list_doc_refs
+
+    def get(id):
+        # Query the collection where 'id' is equal to user_id
+        conversations = conversations_collection.where('id', '==', id).limit(1).stream()
+
+        # Conversations is an iterator of DocumentSnapshot
+        # Converting DocumentSnapshot to a dictionary
+
+        conversations_list = [conversation.to_dict() for conversation in conversations]
+        # NOTE: collection_ref.where returns an array of documents.
+        # We only want the first document in the array.
+        conversation = conversations_list[0] if conversations_list else None
+        conversation_doc_ref = convert_doc_refs(conversation)
+        return conversation_doc_ref
+    
+    def get_all_by_user(user_id):
+        # Query the collection where 'user_id' is equal to user_id
+        conversations = conversations_collection.where('user_id', '==', user_id).stream()
+
+        # Conversations is an iterator of DocumentSnapshot
+        # Converting DocumentSnapshot to a dictionary
+
+        conversations_list = [conversation.to_dict() for conversation in conversations]
+        # NOTE: collection_ref.where returns an array of documents.
+        # We only want the first document in the array.
+        conversation = conversations_list if conversations_list else None
+        conversation_doc_ref = convert_doc_refs(conversation)
+        return conversation_doc_ref
+    
+    def new(conversation: ConversationModel):
+        id = conversation.id  # Generate a unique 20-character ID
+        created_at = conversation.created_at  # Get current timestamp
+
+        conversation_data = {
+            'id': conversation.id,
+            'user_id': conversation.user_id,
+            'name': conversation.name,
+            'messages': conversation.messages,
+            'message_groups': conversation.message_groups,
+            'active': conversation.active,
+            'created_at': conversation.created_at,
+            'updated_at':conversation.updated_at
+        }
+
+        conversation_ref = conversations_collection.document(conversation_data["id"])
+        conversation_ref.set(conversation_data)
+
+        # After setting the data, get the document back as a snapshot and convert it to a dictionary
+        conversation_snapshot = conversation_ref.get()
+        if conversation_snapshot.exists:
+            return conversation_snapshot.to_dict()
+        else:
+            return None
+
+    # NOTE: Need to figure out a way to validate that data argument 
+    # can only contain name
+    def update(id, data):
+        update_data = {
+            **data,
+            'updated_at': generate_timestamp()
+        }
+        conversation_ref = conversations_collection.document(id)
+        conversation_ref.update(update_data)
+
+        # Now read the updated document
+        updated_document = conversation_ref.get()
+        if updated_document.exists:
+            conversation_doc_ref = convert_doc_refs(updated_document.to_dict())
+            return conversation_doc_ref
+        else:
+            # Handle the case where the document does not exist
+            return None
+    
+    def delete(id):
+        conversations_ref = conversations_collection.document(id)
+        conversations_ref.delete()
+        return id
+    
+    def deactivate(id):
+        conversation_ref = conversations_collection.document(id)
+        conversation_ref.update({
+            'active': False,
+            'updated_at': generate_timestamp()
+        })
+        # Retrieve the updated document
+        conversation_doc = conversation_ref.get()
+        conversation = conversation_doc.to_dict()
+        # Return the document data as a dictionary
+        return convert_doc_refs(conversation)
+    
+    def activate(id):
+        conversation_ref = conversations_collection.document(id)
+        conversation_ref.update({
+            'active': True,
+            'updated_at': generate_timestamp()
+        })
+        # Retrieve the updated document
+        conversation_doc = conversation_ref.get()
+        conversation = conversation_doc.to_dict()
+        # Return the document data as a dictionary
+        return convert_doc_refs(conversation)
