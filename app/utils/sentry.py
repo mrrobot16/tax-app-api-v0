c4dd6bfa5c7e1dbc375bacfe750f22a2d47ba4da
Hector G <hmgarcia06@gmail.com>
2023-11-11 22:55:17 -0500

    Added sentry initializer and test endpoints

---
+++ app/utils/sentry.py
@@ -0,0 +1,36 @@
+import sentry_sdk
+
+dsn = "https://6109e8b98dd30fb722b3770de8ef54e7@o4506084321984512.ingest.sentry.io/4506210817277952"
+traces_sample_rate = 1.0
+profiles_sample_rate = 1.0
+
+
+def sentry_init():
+    try:
+        sentry_sdk.init(
+            dsn = dsn,
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            traces_sample_rate = traces_sample_rate,
+            # Set profiles_sample_rate to 1.0 to profile 100%
+            # of sampled transactions.
+            # We recommend adjusting this value in production.
+            profiles_sample_rate = profiles_sample_rate,
+        )
+    except Exception as error:
+        print('utils/sentry.py::sentry_init() error:', error)
+        sentry_sdk.capture_exception(error)
+        sentry_sdk.flush()
+        raise error
+    
+def configure_sentry(app):
+    sentry_init()
+        # Test sentry is up and running
+    @app.get("/sentry", tags = ["Sentry #1"])
+    async def sentry():
+        division_by_zero = 1 / 0
+    @app.get("/sentry_is_working", tags = ["Sentry #2"])
+    async def sentry_works():
+        sentry_is_working()
+
+    return
